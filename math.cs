d1c4e73aaa3c7fa74a06c00232a301c4366e780c
Prashant Verma SDE <114382166+sohit17412@users.noreply.github.com>
2024-10-25 10:07:18 +0530

    Add files via upload

---
+++ math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace math
+{
+
+    class math_pra
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            int y = 45;
+            int z = Math.Max(x, y);
+            int min = Math.Min(x, y);
+            Console.WriteLine(z);
+            Console.WriteLine(min);
+
+
+            double sq=Math.Sqrt(y);
+            Console.WriteLine(sq);
+
+            double ab = -4.3;
+            double abs = Math.Abs(ab);
+            Console.WriteLine(abs);//{4.3}
+
+
+            double ro = 9.66;
+            double ron = Math.Round(ro);
+            Console.WriteLine(ron);//{10}
+        }
+
+    }
+}
