5726919c0b21eff34a534c93ad591ed47732ca3e
Prashant Verma SDE <114382166+sohit17412@users.noreply.github.com>
2024-10-25 10:09:33 +0530

    Add files via upload

---
+++ userInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Oper
+{
+    class userInput
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter your age");
+
+
+            // string user = Console.ReadLine();
+
+
+            //Console.WriteLine("hello " + user);
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is "+ age);// you  cannot get the interger from the input it gives error so you have to convert the given input into int32 by using Convert.ToInt(...).
+
+
+
+
+
+        }
+
+    }
+
+}
