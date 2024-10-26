3e8b71bc3eedf32a5e752ab67b8ab04bcefc7c3c
Prashant Verma SDE <114382166+sohit17412@users.noreply.github.com>
2024-10-26 17:39:57 +0530

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace strin
+{
+    class stri
+    {
+        static void Main(string[] args)
+        {
+            /***Interpolation*/
+            string name = "Prashant";
+            string sirname = "Verma";
+
+            string fullname=$"Hello the developer name is {name} {sirname}";
+
+            Console.WriteLine(fullname);
+
+            /**********************************************************/
+
+            // access string characters
+
+            char ch = name[2];
+            Console.WriteLine(ch);//{a}
+
+            int ind = name.IndexOf('s');
+            Console.WriteLine(ind);
+
+
+            // concatination.
+
+            string a1 = "hello";
+            string a2= "mate your program is  working";
+
+            Console.WriteLine(a1 + a2);
+
+            
+
+        }
+    }
+}
