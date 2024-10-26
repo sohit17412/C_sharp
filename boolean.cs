82ff8b72762d7691ce83c257002d040952e81e41
Prashant Verma SDE <114382166+sohit17412@users.noreply.github.com>
2024-10-26 18:02:45 +0530

    Add files via upload

---
+++ boolean.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace boolpractice
+{
+    class boole
+    {
+        static void Main(string[]args)
+        {
+            int x = 21;
+            int y = 56;
+
+            Console.WriteLine(x > y);// it gives the output in boolen form {false}
+            Console.WriteLine(x<y);// {true}
+
+            /****/
+
+
+            if (x >= y)
+            {
+                Console.WriteLine($"{x} is small");
+
+            }
+            else
+            {
+                Console.WriteLine($"{y} is large");
+            }
+
+
+            /**/
+
+            Console.WriteLine(10 == 15); // returns False, because 10 is not equal to 15
+        }
+    }
+}
